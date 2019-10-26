2eda134b7ac416cb66e1e08d2435e72ebe7d38b5
Sam Hood <me@samhood.net>
2019-10-26 02:14:02 +0100

    Further improved lobby UI

---
+++ Assets/LobbyListRow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LobbyListRow : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        gameObject.transform.Find("Selected").gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.transform.Find("Selected").gameObject.SetActive(false);
+    }
+}

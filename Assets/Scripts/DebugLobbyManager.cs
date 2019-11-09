66efab86915a0e605c74db2c225804be1e9cc1c3
Sam Hood <me@samhood.net>
2019-11-09 11:19:34 +0000

    Some packages

---
+++ Assets/Scripts/DebugLobbyManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using Assets.Scripts.Helpers;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DebugLobbyManager : NetworkLobbyManager
+{
+    private bool _lobbyConnected;
+    private bool _hostStarted;
+
+    public static DebugLobbyManager Instance;
+    
+    private LobbyHook _lobbyHook;
+
+    //Client numPlayers from NetworkManager is always 0, so we count (throught connect/destroy in LobbyPlayer) the number
+    //of players, so that even client know how many player there is.
+    [HideInInspector]
+    public int PlayerCount = 0;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        _lobbyHook = GetComponent<LobbyHook>();
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        HostGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    public void HostGame()
+    {
+        if (_hostStarted) return;
+        // TODO Try..catch
+        StartHost();
+        // TODO Try..catch
+        _hostStarted = true;
+
+
+    }
+
+
+    public override void OnStartHost()
+    {
+        Debug.Log("OnStartHost");
+        base.OnStartHost();
+        AddLocalPlayer();
+    }
+    
+    public void AddLocalPlayer()
+    {
+        TryToAddPlayer();
+    }
+    
+    public override bool OnLobbyServerSceneLoadedForPlayer(GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        if (_lobbyHook != null)
+            _lobbyHook.OnLobbyServerSceneLoadedForPlayer(this, lobbyPlayer, gamePlayer);
+
+        return true;
+    }
+    
+    public void StartGame()
+    {
+        foreach (var t in lobbySlots)
+        {
+            var p = t as LobbyPlayer;
+            if (p != null) p.RpcReady();
+
+        }
+    }
+
+
+    /*public override void OnLobbyClientSceneChanged(NetworkConnection conn)
+    {
+        base.OnLobbyClientSceneChanged(conn);
+        Debug.Log("OnLobbyClientSceneChanged");
+        if (SceneManager.GetSceneAt(0).name == lobbyScene)
+        {
+            MenuUi.Instance.Show();
+        }
+        else
+        {
+            MenuUi.Instance.Hide();
+        }
+    }*/
+
+    public override GameObject OnLobbyServerCreateLobbyPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        base.OnLobbyServerCreateLobbyPlayer(conn, playerControllerId);
+        Debug.Log("OnLobbyServerCreateLobbyPlayer");
+        //MenuUi.OnNewLobbyPlayer();
+        var obj = Instantiate(lobbyPlayerPrefab.gameObject);
+
+
+        return obj;
+    }
+}
--- Assets/Scripts/LobbyPlayer.cs
+++ Assets/Scripts/LobbyPlayer.cs
@@ -47,8 +47,13 @@ public class LobbyPlayer : NetworkLobbyPlayer
             //LobbyManager.Instance.SetServerInfo("Connected", LobbyManager.Instance.networkAddress);
             LobbyManager.Instance.OnPlayersNumberModified(1);
         }
-        LobbyPlayerList.Instance.AddPlayer(this);
-        LobbyPlayerList.Instance.DisplayDirectServerWarning(isServer && LobbyManager.Instance.matchMaker == null);
+
+        // If DebugLobbyManager exists, there will be no lobby
+        if (LobbyPlayerList.Instance != null)
+        {
+            LobbyPlayerList.Instance.AddPlayer(this);
+            LobbyPlayerList.Instance.DisplayDirectServerWarning(isServer && LobbyManager.Instance.matchMaker == null);
+        }
 
         if (isLocalPlayer)
         {
@@ -73,8 +78,8 @@ public class LobbyPlayer : NetworkLobbyPlayer
         //ReadyButton.transform.GetChild(0).GetComponent<Text>().color = Color.white;
 
 
-        var lobbyDetails = GameObject.Find("LobbyDetails").GetComponent<LobbyDetails>();
-        lobbyDetails.SetHost(isServer);
+        var lobbyDetails = GameObject.Find("LobbyDetails");
+        if (lobbyDetails != null) lobbyDetails.GetComponent<LobbyDetails>().SetHost(isServer);
 
         SetupLocalPlayer();
     }
@@ -141,6 +146,7 @@ public class LobbyPlayer : NetworkLobbyPlayer
         //when OnClientEnterLobby is called, the loval PlayerController is not yet created, so we need to redo that here to disable
         //the add button if we reach maxLocalPlayer. We pass 0, as it was already counted on OnClientEnterLobby
         if (LobbyManager.Instance != null) LobbyManager.Instance.OnPlayersNumberModified(0);
+        if (DebugLobbyManager.Instance != null) DebugLobbyManager.Instance.StartGame();
     }
 
     //This enable/disable the remove button depending on if that is the only local player or not
@@ -302,7 +308,7 @@ public class LobbyPlayer : NetworkLobbyPlayer
         // remove player from list of players
 
 
-        LobbyPlayerList.Instance.RemovePlayer(this);
+        if (LobbyPlayerList.Instance != null) LobbyPlayerList.Instance.RemovePlayer(this);
         if (LobbyManager.Instance != null) LobbyManager.Instance.OnPlayersNumberModified(-1);
 
         /*int idx = System.Array.IndexOf(_colors, PlayerColor);
